0f23b41efcdd5082e114bfc128e37689852422f1
shreyathakur024 <shreyathakur200402@gmail.com>
2025-08-16 20:24:35 +0530

    updated navbar

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,27 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-amber-50 py-8">
-    <div className="max-w-6xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 text-left"> 
-    <div className=" text-amber-50 w-140 py-0.5">
+    <footer className="bg-gray-800 text-amber-50 py-8 flex justify-evenly">
+    <div className="max-w-6xl mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-40 text-left"> 
+    <div className=" text-amber-50 w-90 py-0.5 ">
         <h1 className="font-bold text-xl">BlogVista</h1>
-        <p className="text-sm mt-2">Your go-to destination for the latest insights,tutorials,and trends in web development and technology</p>
+        <p className="text-sm mt-3 leading-relaxed ">Your go-to destination for the latest insights,tutorials,
+                 and trends in web development and technology</p>
     </div>
-    <div>
-        <h2 className="hover:text-blue-100 text-neutral-400 font-medium mb-2">Contact Us</h2>
+    <div className="w-90">
+        <h2 className="hover:text-blue-100 text-neutral-400 font-medium mb-3">Contact Us</h2>
         <p>shreyathakur200402@gmail.com</p>
         <p>+91 9508671836</p>
         <p>New Delhi</p>
     </div>
 
-    <div className="font-semibold mb-2">
+    <div className="font-semibold mb-3 w-90">
+      <ul className="space-y-1">
             <h2>Quick Link</h2>
-            <p>Home</p>
-            <p>About Us</p>
+            <p><Link className="text-white hover:text-orange-400" to={'/'}>Home</Link></p>
+            <p><Link className="text-white hover:text-orange-400" to={'/about'}>About Us</Link></p>
             <p>Categories</p>
-            <p>Write For Us</p>
+            {/* <Link className="text-white hover:text-orange-400" to={'/'}>Categories</Link> */}
+            <p className="hover:text-orange-400">Write For Us</p>
             <p>Privacy Policy</p>
+      </ul>
      </div>
     </div>
     </footer>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,11 +15,8 @@ const Navbar = () => {
           <Link className="text-orange-400 hover:bg-yellow-300 hover:text-black px-3 py-1 rounded" to={'/'}>Home</Link>
         <Link className="text-orange-400 hover:bg-yellow-300 hover:text-black px-3 py-1 rounded" to={'/about'}>About</Link>
         <Link className="text-orange-400 hover:bg-yellow-300 hover:text-black px-3 py-1 rounded" to={'/contact'}>Contact</Link>
-        <Link className="text-orange-400 hover:bg-yellow-300 hover:text-black px-3 py-1 rounded" to={'/articles'}>Articles</Link>
-        <button onClick={()=>setSearch(!search)}>
-          <FaSearch className="search text-amber-50 text-2xl hover:text-yellow-300 " />
-          </button>
-          {search && (
+        <Link className="text-orange-400 hover:bg-yellow-300 hover:text-black px-3 py-1 rounded" to={'/articles'}>Articles </Link>      
+        {search && (
             <input type="text" placeholder="Search..." className="md:flex-shrink-0 w-40 sm:w-56 top-8 right-0 border rounded px-2 py-1 bg-blue-150 text-cyan-50"/>
           )}
         </div>
